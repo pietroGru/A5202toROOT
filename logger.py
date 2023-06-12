f2f778ecde699b376f877c2d861b4dac8261761b
Pietro Grutta <pietrogrutta@gmail.com>
2023-06-12 15:28:23 +0200

    New logger class (with status function)

--- logger.py
+++ logger.py
@@ -1,28 +1,51 @@
 import logging as loglib
+import time
 
+##############################################################
+######## globals #############################################
+##############################################################
 # Configure logger
 class CustomFormatter(loglib.Formatter):
     grey = "\x1b[38;20m"
     yellow = "\x1b[33;20m"
     red = "\x1b[31;20m"
     bold_red = "\x1b[31;1m"
+    green = "\x1b[32;1m"
     reset = "\x1b[0m"
-    time = "[%(asctime)s]"
-    format = " %(levelname)s "
-    message = "- %(message)s"
+    timeStr = "[%(asctime)s]"
+    formatStr = " %(levelname)s "
+    messageStr = "- %(message)s"
     FORMATS = {
-        loglib.DEBUG: time + grey + format + reset + message,
-        loglib.INFO: time + grey + format + reset + message,
-        loglib.WARNING: time + yellow + format + reset + message,
-        loglib.ERROR: time + red + format + reset + message,
-        loglib.CRITICAL: time + bold_red + format + reset + message
-    }
+        loglib.DEBUG: timeStr + grey + formatStr + reset + messageStr,
+        loglib.INFO: timeStr + grey + formatStr + reset + messageStr,
+        loglib.WARNING: timeStr + yellow + formatStr + reset + messageStr,
+        loglib.ERROR: timeStr + red + formatStr + reset + messageStr,
+        loglib.CRITICAL: timeStr + bold_red + formatStr + reset + messageStr,
+    }         
     def format(self, record):
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = loglib.Formatter(log_fmt, datefmt='%H:%M:%S')
         return formatter.format(record)
+
+
+class luxeLogging(loglib.Logger):
+    def status(self, message):
+        #return
+        green = "\x1b[32;1m"
+        reset = "\x1b[0m"
+        header = f"[{time.strftime('%H:%M:%S')}] {green}STATUS{reset} - {message}"
+        print(header, end="\r", flush=True)
+        
+    def statusRed(self, message):
+        #return
+        bold_red = "\x1b[31;1m"
+        reset = "\x1b[0m"
+        header = f"[{time.strftime('%H:%M:%S')}] {bold_red}STATUS{reset} - {message}"
+        print(header, end="\r", flush=True)
+
 # create logger with 'spam_application'
-logging = loglib.getLogger("rootConverter")
+#logging = loglib.getLogger("rootConverter")
+logging = luxeLogging("rootConverter", loglib.INFO)
 logging.setLevel(loglib.INFO)
 # create console handler with a higher log level
 ch = loglib.StreamHandler()
