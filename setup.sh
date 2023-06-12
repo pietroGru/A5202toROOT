7ea46fa531b088df25ab10b708ecf16c4cbc9388
Pietro Grutta <pietrogrutta@gmail.com>
2023-06-12 15:29:09 +0200

    shell to run locally

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# Setup the local mount of the CLEAR_Vesper
+sshfs cern_cleardaq:/home/pietro/work/ /home/pietro/clearDaq/
+# Setup the port forwarding for the socket
+#screen -S portForward -d -m ssh -R 65432:localhost:65432 -R 50007:localhost:50007 cern_cleardaq
