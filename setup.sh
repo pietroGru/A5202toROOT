21fdca3ebf38fe1b73480715644b04682620a795
Pietro Grutta <pietrogrutta@gmail.com>
2023-10-07 17:15:48 +0200

    Copy sym link for logger and rdataStruct

--- setup.sh
+++
@@ -1,4 +0,0 @@
-# Setup the local mount of the CLEAR_Vesper
-sshfs cern_cleardaq:/home/pietro/work/ /home/pietro/cleardaq/
-# Setup the port forwarding for the socket
-#screen -S portForward -d -m ssh -R 65432:localhost:65432 -R 50007:localhost:50007 cern_cleardaq
