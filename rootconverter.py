537f11c4db51ef6b1f2e170a8cc50a5cc1296600
Pietro Grutta <pietrogrutta@gmail.com>
2023-10-13 13:23:08 +0200

    cal paths

--- rootconverter.py
+++ rootconverter.py
@@ -19,7 +19,7 @@ class rootconverter():
     # rootconverter logger
     logging = create_logger("rootconverter")
     
-    def __init__(self, path, outputROOTdirectory, calPath="/home/pietro/work/CLEAR_March/data/fers/calibrations/CERN_Vesper_4_withPedestals.root", rfileModeVector = True) -> None:
+    def __init__(self, path, outputROOTdirectory, calPath="data/fers/calibrations/CERN_Vesper_4_wPed.root", rfileModeVector = True) -> None:
         """
         FERS ASCII (list + info) to ROOT converter class 
         
